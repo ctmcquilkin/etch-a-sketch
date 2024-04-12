9762ca87730242742d73f177163a34bd36deec48
ctmcquilkin <ctmcquilkin@gmail.com>
2024-04-11 22:39:49 -0500

    Refactored javascript to add event listeners, set color function, remove color function and get random color function and color array. Adjusted CSS to match dark style.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,39 @@
 window.onload = function() {
+    const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
     const container = document.querySelector('#container');
-    var i = 0;
-    var j = 0;
+    let i = 0;
+    let j = 0;
 
     for (i = 1; i <= 4; i++) {
         for (j = 1; j <= 4; j++) {
-            var d = document.createElement('div');
-            container.appendChild(d);
+            const square = document.createElement('div');
+            square.classList.add('square');
+
+            square.addEventListener('mouseover', () => {
+                setColorToEl(square);
+            });
+
+            square.addEventListener('mouseout', () => {
+                removeColorFromEl(square);
+            });
+
+            container.appendChild(square);
         };
     };
+
+    function setColorToEl(element) {
+        const color = getRandomColor();
+        element.style.background = color;
+        element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+    };
+
+    function removeColorFromEl(element) {
+        element.style.background = '#1D1D1D';
+        element.style.boxShadow = `0 0 2px #000`;
+    }
+
+    function getRandomColor() {
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+
 };
